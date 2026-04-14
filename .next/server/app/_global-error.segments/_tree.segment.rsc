c949af092f4341ed4d516d7e5bed2d0670fc349e
netal.gupta <netal.gupta@somaiya.edu>
2026-04-15 00:25:54 +0530

    app build

--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++ .next/server/app/_global-error.segments/_tree.segment.rsc
@@ -1,1 +1,1 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":0,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"9cJn6jjcJ6AM14fIP6CdU"}
+0:{"tree":{"name":"","param":null,"prefetchHints":0,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"CdDOXlM_72rqq_uqgukJZ"}
--- .next/server/app/auth/callback/route.js
+++ .next/server/app/auth/callback/route.js
@@ -1,5 +1,6 @@
 var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/auth/callback/route.js")
-R.c("server/chunks/[root-of-the-server]__0ppfij_._.js")
+R.c("server/chunks/[externals]_next_dist_0sqmaqd._.js")
+R.c("server/chunks/node_modules_062hqs-._.js")
 R.c("server/chunks/node_modules_@supabase_supabase-js_dist_index_mjs_0hp37pu._.js")
 R.c("server/chunks/[root-of-the-server]__0j8-xkl._.js")
 R.c("server/chunks/_next-internal_server_app_auth_callback_route_actions_09jr~mp.js")
